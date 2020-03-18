41929ec18470090ef809bb350abec7d27639a6a7
scaicedo27 <45157348+scaicedo27@users.noreply.github.com>
2020-03-18 15:29:28 -0500

    App

---
+++ src/app/services/query.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Clientmodel} from './clientmodel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QueryService {
+
+  selectclients: Clientmodel;
+  client: Clientmodel[];
+
+  readonly URL_API_CREATE_TRANSACTION = 'http://crudformongo-workshopdemo.ocpcloudpakapp-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers';
+  readonly URL_API_GET_TRANSACTIONS = 'http://crudformongo-workshopdemo.ocpcloudpakapp-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers';
+  readonly URL_API_DELETE = 'http://crudformongo-workshopdemo.ocpcloudpakapp-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers/';
+
+  constructor(public httpClient: HttpClient)
+  {
+    this.selectclients = new Clientmodel();
+  }
+
+  public getClient()
+  {
+    return this.httpClient.get(this.URL_API_GET_TRANSACTIONS);
+  }
+
+  public createTransaction(form)
+  {
+    return this.httpClient.post( this.URL_API_CREATE_TRANSACTION, form);
+  }
+
+  public deleteTransaction(id)
+  {
+    return this.httpClient.delete(this.URL_API_DELETE + id );
+  }
+
+}
